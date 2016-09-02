948eb598e23dabf2ced95cf7e81a3d59bd03a0a3
Attila Herbert <herbertatus@gmail.com>
2016-09-02 14:03:57 +0200

    fixed drawing of map

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 var insert = require('./insert');
 
 const STEP = 50;
-const WIDTH = 200, HEIGHT = 100;
+const WIDTH = 100, HEIGHT = 100;
 var ptr  = [[0, 1, 0, 0, 0, 0, 0],
             [0, 0, 0, 1, 0, 0, 0],
             [1, 1, 0, 0, 1, 1, 1]];
@@ -22,19 +22,13 @@ function init(scr) {
 }
 
 function draw(cwObj, pattern, x = 0, y = 0) {
-    if(pattern){//draw pattern to map
+    if(pattern){
         pattern.map(function (cur, i){
             cwObj.gMap[x + i] = insert(cwObj.gMap[x + i], cur, y);
-            //insert() is a helper, just inserts at an index
         });
     }
-    var out = '';
-    cwObj.gMap.map(function (cur) {//create a string of active and
-        cur.map(function (cell) {  //inactive cells
-            if(cell) {out += "<b>&#9632;</b>";} else {out += "&#9632;";}
-        });
-        out += "<br>";
-    });
+    const out = cwObj.gMap.reduce((prev, cur) =>
+        prev + cur.map(c => c ? '<b>&#9632;</b>' : '&#9632;').join('') + '<br>', '');
     cwObj.canvas.innerHTML = out;  //put it in the HTML
 }
 
@@ -43,7 +37,6 @@ function step (gm){
         return cur.map(function (cell, x){
             var n = getNb(gm, x, y);
             n = cell === 1 ? n - 1 : n;//getNb includes the cell itself
-            console.log(n);
             return n === 3 ? 1 : ((n === 2 && cell === 1) ? 1 : 0);//rules
         });
     });
